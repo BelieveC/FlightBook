41de97fe8fc71e6602a3c73b306472a8414073c2
Chaitanya Yadav <cayadav2009@gmail.com>
2020-06-06 17:10:25 +0530

    Updates

---
+++ precache-manifest.c93d213e39e1a91c92c4a62aef316aeb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7ce430ad7894bd02262",
+    "url": "/FlightBook/static/js/runtime~main.7c0d13e3.js"
+  },
+  {
+    "revision": "53ac4171d05a0012fa5a",
+    "url": "/FlightBook/static/js/main.e62400b4.chunk.js"
+  },
+  {
+    "revision": "4e8bf18f650ddad53515",
+    "url": "/FlightBook/static/js/2.78c5f89d.chunk.js"
+  },
+  {
+    "revision": "1265fbc2cf06cfee163189fe44ccd63b",
+    "url": "/FlightBook/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/FlightBook/precache-manifest.6725b63e1daeebf1ff29bff102778ec5.js"
+  "/FlightBook/precache-manifest.c93d213e39e1a91c92c4a62aef316aeb.js"
 );
 
 workbox.clientsClaim();
