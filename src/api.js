bf0b0968b536a4413a32994a8570393e99970f7f
Chaitanya Yadav <cayadav2009@gmail.com>
2025-01-09 13:28:36 +0530

    refactor(api): improve code formatting and update API endpoint URL

--- src/api.js
+++ src/api.js
@@ -1,18 +1,20 @@
-import Ajax from './utils/Ajax'
+import Ajax from "./utils/Ajax";
 
 const Api = {
   fetchNiftyIndices(indexSymbol) {
-    const endDate = new Date()
-    const endDateUnix = parseInt(endDate.getTime()/1000).toFixed(0)
+    const endDate = new Date();
+    const endDateUnix = parseInt(endDate.getTime() / 1000).toFixed(0);
     // const startDate = new Date(endDate.getFullYear() - 1, endDate.getMonth() + 1, endDate.getDate())
     var startDate = new Date();
     startDate.setMonth(startDate.getMonth() - 2);
-    const startDateUnix = parseInt(startDate.getTime()/1000).toFixed(0)
+    const startDateUnix = parseInt(startDate.getTime() / 1000).toFixed(0);
 
-    return Ajax.get(`https://finnhub.io/api/v1/stock/candle?symbol=${indexSymbol}&resolution=D&from=${startDateUnix}&to=${endDateUnix}&token=${process.env.REACT_APP_ACCESS_TOKEN}`)
-                    .then(response => ({ response }))
-                    .catch(error => ({ error }))
-  }
-}
+    return Ajax.get(
+      `https://fintech.io/api/v1/stock/candle?symbol=${indexSymbol}&resolution=D&from=${startDateUnix}&to=${endDateUnix}&token=${process.env.REACT_APP_ACCESS_TOKEN}`
+    )
+      .then((response) => ({ response }))
+      .catch((error) => ({ error }));
+  },
+};
 
-export default Api
+export default Api;
